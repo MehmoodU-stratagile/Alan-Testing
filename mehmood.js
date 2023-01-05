ab89c83e72ccebde9bd1221a810e2d939b5ea307
Mehmood Usman <36883202+MehmoodU-stratagile@users.noreply.github.com>
2023-01-05 12:29:22 +0500

    first command is done

--- mehmood.js
+++ mehmood.js
@@ -2,3 +2,17 @@
 intent('hello world', p => {
     p.play('(hello|hi there)');
 });
+
+
+intent('(increment|do increment)',p=>{
+    p.play({command:'increment'})
+})
+
+intent('(back|go back)',p=>{
+    p.play({command:'back'})
+})
+
+
+intent('(forward|go forward)',p=>{
+    p.play({command:'forward'})
+})
